b09d836219a57dad88b9020f4560cc0f6245457b
Sarang19 <rastogi.sarang2004@gmail.com>
2026-03-15 12:30:29 +0530

    updated favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" type="image/png" href="/assets/react.svg" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Mona+Sans:ital,wght@0,200..900;1,200..900&display=swap"
           rel="stylesheet"
