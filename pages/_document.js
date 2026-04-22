93babc4c3e43cf13d51c131ce214d0f76ffd6eba
Sarang19 <rastogi.sarang2004@gmail.com>
2026-04-22 14:21:14 +0530

    feat: add custom document with google site verification

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="N2OQqKevy-vn8wGEnDJGLBezjVJEwWqKDscr3EsJziY" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Mona+Sans:ital,wght@0,200..900;1,200..900&display=swap"
